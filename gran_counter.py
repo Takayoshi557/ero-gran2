951128ab6f4b53606e7bb4d37eb42be79f2ff217
Takayoshi555 <64679300+Takayoshi555@users.noreply.github.com>
2020-08-09 23:23:56 +0900

    Update gran_counter.py

--- gran_counter.py
+++ gran_counter.py
@@ -9,7 +9,7 @@ import csv
 import os
 import math
 import random
-import gspread
+#import gspread
 #import json
 
 
@@ -36,17 +36,17 @@ async def on_ready():
     
     
 #ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 
 #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+#scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-credentials = ServiceAccountCredentials.from_json_keyfile_name('camarade-secret_key.json', scope)
+#credentials = ServiceAccountCredentials.from_json_keyfile_name('camarade-secret_key.json', scope)
 
 #OAuth2の資格情報を使用してGoogle APIにログインします。
-gc = gspread.authorize(credentials)
+#gc = gspread.authorize(credentials)
 
 #共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
 #SPREADSHEET_KEY = '1HsQ_p2Hsg2g4tb8bXClOqseIhCYoI-4-FaWNrlktdnE'
