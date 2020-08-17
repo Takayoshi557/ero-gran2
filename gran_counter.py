5c60cd81c64b0f87a4beb2bca5282e8a6510a0d4
Takayoshi555 <64679300+Takayoshi555@users.noreply.github.com>
2020-08-17 16:12:01 +0900

    Update gran_counter.py

--- gran_counter.py
+++ gran_counter.py
@@ -58,7 +58,18 @@ async def on_raw_reaction_add(payload):
 #async def on_raw_reaction_add(reaction, user):
 #    with open('gran_log.txt', 'a', newline='') as f:
     if not payload.channel_id == 732658643740262553:
-        return
+        if not payload.channel_id == 744727455293767711:
+            return
+        else:
+            channel = client.get_channel(722253361159864479)
+            worksheet_find = gc.open_by_key(SPREADSHEET_KEY).worksheet('rare(red,purple)')
+            search_mid = payload.message_id
+            mid_cell = worksheet_find.find(str(search_mid))
+            entry_num = worksheet_find.cell(mid_cell.row, 165).value
+            entry_col = int(entry_num) + int(11)
+            worksheet_find.update_cell(mid_cell.row, int(entry_col), str(payload.user_id))
+            await channel.send('なぜに？')
+            return
     else:    
         channel = client.get_channel(722253361159864479)
         now = dt.now()
