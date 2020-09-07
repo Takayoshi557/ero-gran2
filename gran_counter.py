d0ba6871bd7e18636551d43552a9c8c3e4249179
Takayoshi555 <64679300+Takayoshi555@users.noreply.github.com>
2020-09-07 13:37:50 +0900

    Update gran_counter.py

--- gran_counter.py
+++ gran_counter.py
@@ -382,7 +382,7 @@ async def on_message(message):
                 get_name = worksheet_find.cell(cell_list[num].row, 5).value
                 get_date = worksheet_find.cell(cell_list[num].row, 6).value
                 r_list.append(get_id + '\t: ' + get_boss + '\t/ ' + get_item + '\t/ ' + get_name + '\t/ ' + get_date)
-                await asyncio.sleep(4)
+                await asyncio.sleep(5)
                 if num == 20:
                     r_list = '\n'.join(r_list)
                     get_r = discord.Embed(title='DROP ITEM LIST (GRADE: ALL)',
@@ -391,8 +391,9 @@ async def on_message(message):
                     get_r.add_field(name='---------------------------------------------', value=str(r_list),
                                     inline=True)
                     await list_channel.send(embed=get_r)
-                    await list_channel.send('以下にまだ続きます。もうしばらくお待ちください。')
+                    await list_channel.send('以下にまだ続きます。listコマンドを入力せず、もうしばらくお待ちください。')
                     r_list = list()
+                    await asyncio.sleep(5)
             r_list = '\n'.join(r_list)
             get_r = discord.Embed(title='DROP ITEM LIST (GRADE: ALL)',
                                   description='ID \t:\t  boss \t/  item \t/  holder \t/  date',
